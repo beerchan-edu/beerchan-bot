048bae19574949d90295ffb7d818a351b48d10e1
Nix Silvam <nixsilvam@icloud.com>
2024-02-19 16:35:12 +0200

    Added logging and more logics

--- app/bot.py
+++ app/bot.py
@@ -1,45 +1,40 @@
-from asyncio import run
-from command_handler import hello_command, save_message, test_result
-from db import async_engine, Base
+import logging
+from command_handler import hello_command, test_result, save_message
+from db import engine, Base, Session
 from telegram.ext import (
     Application,
     CommandHandler,
 )
 from consts import TELEGRAM_KEY
 
-async def create_tables():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
-async def main():
-    print("Creating tables")
-    await create_tables()
+Base.metadata.create_all(engine)
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+def main():
 
     print("Starting bot")
     app = Application.builder().token(TELEGRAM_KEY).build()
 
     app.add_handler(CommandHandler("hello", hello_command))
-    app.add_handler(CommandHandler("insert", save_message))
     app.add_handler(CommandHandler("result", test_result))
+    app.add_handler(CommandHandler("save", save_message))
 
     print("Polling")
     app.run_polling(poll_interval=3)
 
 
 if __name__ == "__main__":
-    run(main())
-    # print("Starting bot")
-    # app = Application.builder().token(TELEGRAM_KEY).build()
-
-    # # # run(create_tables())
-    # # Base.metadata.create_all(async_engine)
-
-    # # commands
-    # app.add_handler(CommandHandler("hello", hello_command))
-    # app.add_handler(CommandHandler("insert", save_message))
-    # app.add_handler(CommandHandler("result", test_result))
-
-    # print("Polling")
-    # app.run_polling(poll_interval=3)
+    main()
 
 
