15090719d094d865dffde3f396719164f6ad8ddf
Nix Silvam <nixsilvam@icloud.com>
2024-02-19 16:36:44 +0200

    command_handler changes

--- app/command_handler.py
+++ app/command_handler.py
@@ -1,42 +1,39 @@
-from db import async_session
+from db import Session
 from models import Sport
 from telegram import Update
 from telegram.ext import ContextTypes
-from sqlalchemy import select
+import re
+from datetime import timedelta
 
-
-async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):   
-    await update.message.reply_text(f"Hi {update.effective_user.name}!")
+async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user.name  
+    await update.message.reply_text(f"Hi {user}! I write your workout duration in minutes.")
 
 
 async def save_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    async with async_session() as session:
-        async with session.begin():
-            try:
-                await update.message.reply_text("Try write smth")
-                message_text = update.message.text
-                add_message = Sport(message=message_text)
-                add_username = Sport(nickname=update._effective_user.name)  
-                session.add_all(add_message, add_username)
-            except:
-                await session.rollback()
-            finally:
-                await update.message.reply_text("You fukin did it!")
+    with Session() as session:
+        with session.begin():
+            message_text = update.message.text
+            digits = parseNumber(message_text)
+            if digits:                
+                add_message = Sport(nickname=update._effective_user.name, \
+                    user_id=update.effective_user.id, chat_id=update.effective_chat.id,\
+                        duration=timedelta(minutes=digits), message=message_text.replace("/save ", ""))
+                session.add(add_message)
+            else:
+                await update.message.reply_text("Write a command in correct format: mm")
 
 
-async def test_result(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    async with async_session() as session:
-        async with session.begin():
-            active_username = update._effective_user.name
-            try:
-                stmt = select(Sport).where(Sport.nickname == active_username)
-                results = await session.execute(stmt)
-                for result in results:
-                    await update.message.reply_text(result)
-            except:
-                session.rollback()
-            finally:
-                session.commit()    
+async def test_result(update: Update, context: ContextTypes.DEFAULT_TYPE): # not working anymore
+    with Session() as session:
+        with session.begin():
+            user = update.effective_user.name
+            sports = Sport.get_query(session, user)()
+            for sport in sports:
+                result = f"{sport.nickname} / {sport.message}"
+                await update.message.reply_text(f"{result}")
 
-                
 
+def parseNumber(string: str):
+    matches = re.findall(r'\b\d{1,2}\b', string)
+    return int(matches[0]) if len(matches) == 1 else None
