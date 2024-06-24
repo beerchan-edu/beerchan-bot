65f9d1f2c06ced9f42d6af62f0752f44d94202d2
Nix Silvam <nixsilvam@icloud.com>
2024-06-24 12:15:25 +0300

    changed parseNumber, reformated, fixed

--- app/command_handler.py
+++ app/command_handler.py
@@ -5,35 +5,57 @@ from telegram.ext import ContextTypes
 import re
 from datetime import timedelta
 
+
 async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user.name  
-    await update.message.reply_text(f"Hi {user}! I write your workout duration in minutes.")
+    user = update.effective_user.name
+    await update.message.reply_text(
+        f"Hi {user}! I write your workout duration only in minutes."
+    )
 
 
 async def save_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with Session() as session:
         with session.begin():
+            user = update.effective_user.name
             message_text = update.message.text
             digits = parseNumber(message_text)
-            if digits:                
-                add_message = Sport(nickname=update._effective_user.name, \
-                    user_id=update.effective_user.id, chat_id=update.effective_chat.id,\
-                        duration=timedelta(minutes=digits), message=message_text.replace("/save ", ""))
+            if digits is not False:
+                message_text = re.sub(
+                    r"/save\s+|/save@KhargolGroBogukBot\s+", "", message_text
+                )
+                add_message = Sport(
+                    nickname=user,
+                    user_id=str(update.effective_user.id),
+                    chat_id=str(update.effective_chat.id),
+                    duration=timedelta(minutes=digits),
+                    message=message_text,
+                )
                 session.add(add_message)
+                await update.message.reply_text(f"I saved your result, {user}")
             else:
-                await update.message.reply_text("Write a command in correct format: mm")
+                await update.message.reply_text(
+                    "Write a command in correct format only in minutes: mm"
+                )
 
 
-async def test_result(update: Update, context: ContextTypes.DEFAULT_TYPE): # not working anymore
+async def show_best_sportsman(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with Session() as session:
         with session.begin():
-            user = update.effective_user.name
-            sports = Sport.get_query(session, user)()
-            for sport in sports:
-                result = f"{sport.nickname} / {sport.message}"
-                await update.message.reply_text(f"{result}")
+            chat_id = str(update.effective_chat.id)
+            results = Sport.best_query(session, chat_id)
+            number = 0
+            result_message = "TOP best sportsmen for the last 30 days:\n"
+            for result in results:
+                number += 1
+                total_seconds = result.total_duration.total_seconds()
+                hours = int(total_seconds // 3600)
+                minutes = int((total_seconds % 3600) // 60)
+                row_message = f"{number}. {result.nickname} {hours}h {minutes}m\n"
+                result_message += row_message
+
+            await update.message.reply_text(f"{result_message}")
 
 
 def parseNumber(string: str):
-    matches = re.findall(r'\b\d{1,2}\b', string)
-    return int(matches[0]) if len(matches) == 1 else None
+    matches = re.findall(r"\d{1,3}", string)
+    return int(matches[0]) if len(matches) == 1 else False
