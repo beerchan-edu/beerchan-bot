839e865653c936a046c3346421c9a32a1cdaf989
Nix Silvam <nixsilvam@icloud.com>
2024-06-24 12:13:16 +0300

    foxed tests for pne func

--- app/test_parseNumber.py
+++ app/test_parseNumber.py
@@ -3,36 +3,13 @@ import pytest
 
 
 def test_parseNumber_single_digit():
-    assert parseNumber("I made workout for 5 mins") == 5
-
-def test_parseNumber_two_digits():
-    assert parseNumber("57 minutes") == 57
-
-def test_parseNumber_single_digit_with_text_after():
-    assert parseNumber("5minutes") is None
-
-def test_parseNumber_two_digits_with_text_after():
-    assert parseNumber("57minutes") is None
-
-def test_parseNumber_single_digit_with_text_before():
-    assert parseNumber("I made workout for5 mins") is None
-
-def test_parseNumber_two_digits_with_text_before():
-    assert parseNumber("I made workout for57 mins") is None
-
-def test_parseNumber_single_digit_with_text_around():
-    assert parseNumber("I made workout for5mins") is None
-
-def test_parseNumber_two_digits_with_text_around():
-    assert parseNumber("I made workout for57mins") is None
-
-def test_parseNumber_more_than_two_digits():
-    assert parseNumber("I made workout for 579 mins") is None
-    assert parseNumber("I made workout for 5791 mins") is None
-
-def test_parseNumber_multiply_matches():
-    assert parseNumber("I made workout for 57, 1.") is None
-    assert parseNumber("I made workout for 1 hour and 57 minutes") is None
-
-def test_parseNumber_no_match():
-    assert parseNumber("I made workout fifty seven minutes") is None
+    dict = {
+        "I made workout for 5 mins": 5,
+        "57 minutes": 57,
+        "I made workout for575mins": 575,
+        "5678 minutes": False,
+        "Sentense without digits": False,
+        "1 hour 30 mins": False,
+    }
+    for k, v in dict.items():
+        assert parseNumber(k) == v
