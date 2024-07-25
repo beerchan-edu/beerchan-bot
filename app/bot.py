47695a9ef40099091cf1489b12334a8b6eea781d
Nix Silvam <nixsilvam@icloud.com>
2024-07-25 21:15:01 +0300

    made a class for command handler

--- app/bot.py
+++ app/bot.py
@@ -1,10 +1,5 @@
 import logging
-from command_handler import (
-    hello_command,
-    save_message,
-    show_best_sportsman,
-    update_username,
-)
+from command_handler import CommandHandler as TGCommandHandler
 from app.db import engine, Base, Session
 from telegram.ext import (
     Application,
@@ -15,15 +10,14 @@ from consts import TELEGRAM_KEY
 
 Base.metadata.create_all(engine)
 
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
+
+cmd_handler = CommandHandler()
 
 
 def main():
@@ -31,10 +25,11 @@ def main():
     print("Starting bot")
     app = Application.builder().token(TELEGRAM_KEY).build()
 
-    app.add_handler(CommandHandler("hello", hello_command))
-    app.add_handler(CommandHandler("save", save_message))
-    app.add_handler(CommandHandler("top", show_best_sportsman))
-    app.add_handler(CommandHandler("update_username", update_username))
+    app.add_handler(CommandHandler("hello", cmd_handler.hello_command))
+    app.add_handler(CommandHandler("help", cmd_handler.help_command))
+    app.add_handler(CommandHandler("save", cmd_handler.save_message))
+    app.add_handler(CommandHandler("top", cmd_handler.show_best_sportsman))
+
 
     print("Polling")
     app.run_polling(poll_interval=3)
