e2f78897da6225f66373dfd4a64bd987cc6b5c75
Nix Silvam <nixsilvam@icloud.com>
2024-07-26 17:32:37 +0300

    docker

--- app/command_handler.py
+++ app/command_handler.py
@@ -5,17 +5,13 @@ from telegram.ext import ContextTypes
 import re
 from datetime import timedelta
 
-
 class CommandHandler:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     async def hello_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.effective_user.name
-        await update.message.reply_text(
-            f"Hi {user}! I write your workout duration only in minutes."
-        )
-
+        await update.message.reply_text(f"Hi {user}! I write your workout duration only in minutes.")
 
     async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         help_text = (
@@ -23,17 +19,16 @@ class CommandHandler:
             "/start - Start the bot and get a welcome message\n"
             "/help - Get information about available commands\n"
             "/save <message> - Save your workout message (duration in minutes)\n"
-            "/best - Show the top sportsmen for the last 30 days\n"
+            "/top - Show the top sportsmen for the last 30 days\n"
         )
         await update.message.reply_text(help_text)
-
 
     async def save_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         with Session() as session:
             with session.begin():
                 user = update.effective_user.name
                 message_text = update.message.text
-                digits = self.parseNumber(message_text)
+                digits = self.parse_number(message_text)
                 if digits is not False:
                     message_text = re.sub(
                         r"/save\s+|/save@KhargolGroBogukBot\s+", "", message_text
@@ -52,32 +47,26 @@ class CommandHandler:
                         "Write a command in correct format only in minutes: mm"
                     )
 
-
     async def show_best_sportsman(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         with Session() as session:
             with session.begin():
                 chat_id = str(update.effective_chat.id)
                 results = Sport.best_query(session, chat_id)
-                number = 0
-                result_message = "TOP best sportsmen for the last 30 days:\n"
-                for result in results:
-                    number += 1
-                    total_seconds = result.total_duration.total_seconds()
-                    hours, minutes = self.parseTime(total_seconds)
-                    row_message = f"{number}. {result.nickname} {hours}h {minutes}m\n"
-                    result_message += row_message
+                if results:
+                    number = 0
+                    result_message = "TOP best sportsmen for the last 30 days:\n"
+                    for result in results:
+                        number += 1
+                        total_seconds = result.total_duration.total_seconds()
+                        hours = int(total_seconds // 3600)
+                        minutes = int((total_seconds % 3600) // 60)
+                        row_message = f"{number}. {result.nickname} {hours}h {minutes}m\n"
+                        result_message += row_message
+                    await update.message.reply_text(f"{result_message}")
+                else:
+                    await update.message.reply_text("No workout records found for the last 30 days.")
 
-                await update.message.reply_text(f"{result_message}")
-
-
-    @staticmethod        
-    def parseNumber(user_string: str):
+    @staticmethod
+    def parse_number(user_string: str):
         matches = re.findall(r"\d{1,3}", user_string)
         return int(matches[0]) if len(matches) == 1 else False
-    
-    
-    @staticmethod
-    def parseTime(total_seconds: str):
-        hours = int(total_seconds // 3600)
-        minutes = int((total_seconds % 3600) // 60)
-        return hours, minutes
--- app/db.py
+++ app/db.py
@@ -1,16 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-config = dotenv_values("./.env")
-username = config.get("DB_USERNAME")
-password = config.get("DB_PASSWORD")
-dbname = config.get("DB_NAME")
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+dbname = os.getenv("POSTGRES_DB")
+host = "db"  
 
-engine = create_engine(f"postgresql+psycopg://{username}:{password}@localhost:5432/{dbname}", echo=True)
+engine = create_engine(f"postgresql+psycopg://{username}:{password}@{host}:5432/{dbname}", echo=True)
 Session = sessionmaker(bind=engine, expire_on_commit=False)
-
 
 class Base(DeclarativeBase):
     pass
